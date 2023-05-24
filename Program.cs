a143d8ad1e5dc34fcb177a9679b7840ab4f45aae
Hollow1 <113149993+Ho11ow1@users.noreply.github.com>
2023-05-24 20:52:35 +0200

    Add files via upload

---
+++ Program.cs
@@ -0,0 +1,98 @@
+﻿// Import libraries
+
+
+
+// ==================== VARIABLE DECLARATIONS ====================
+
+/*
+
+int = whole number    int num = 5;
+double = decimal      double num = 5.5;
+char = single character     char letter = 'a'; )
+string = text       string text = " Hollow ";
+bool = states       bool Boolean = true;  { true = 1    false = 0 }
+
+*/
+
+// ==================== ARITHMETIC OPERATORS ====================
+
+/*
+
+=   is      x=5
+==  Equal to	x == y
+!=	Not equal	x != y
+>	Greater than	x > y
+<	Less than	x < y
+>=	Greater than or equal to	x >= y
+<=	Less than or equal to	x <= y
+&&  And also     a > b && c < d
+
+*/
+
+// ==================== WHILE LOOP HOW TO ====================
+
+/*
+
+    int i = 0;
+
+    while (i < 5) 
+    {
+        Console.WriteLine (i++);
+    }
+
+*/
+
+// ==================== FOR LOOP HOW TO ====================
+
+/*
+
+    for (int i = 0; i < 5;)
+    {
+        Console.WriteLine(i++);
+    }
+
+*/
+
+// ==================== SWITCH CASE HOW TO ====================
+
+/*
+
+    int day = 4;
+
+    switch (day)
+    {
+        case 1:
+            Console.WriteLine ("Monday");
+            break;
+        case 2:
+            Console.WriteLine("Tuesday");
+            break;
+        case 3:
+            Console.WriteLine("Wednesday");
+            break;
+        case 4:
+            Console.WriteLine("Thursday");
+            break;
+        case 5:
+            Console.WriteLine("Friday");
+            break;
+        case 6:
+            Console.WriteLine("Saturday");
+            break;
+        case 7:
+            Console.WriteLine("Sunday");
+            break;
+    }
+
+*/
+
+// ==================== CREATE AN ARRAY ====================
+
+/*
+
+    string[] cars = { "VW", "VW", "VW", "VW" };
+
+*/
+
+
+Console.WriteLine ("Hello World!");
